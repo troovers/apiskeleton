bd51b1b281cce5c9f307b0883a087cf3adf848f8
Thomas Roovers <troovers@e-sites.nl>
2019-01-18 11:09:34 +0100

    Added default files for Gitlab CI + .env

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/config/bootstrap.php';
 
+umask(0002);
+
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 
--- src/Scripts.php
+++ src/Scripts.php
@@ -2,16 +2,46 @@
 
 namespace App;
 
-use Composer\Script\Event;
 use Composer\Composer;
+use Composer\Script\Event;
 
 class Scripts
 {
 	/**
 	 * @param $event
+	 * @throws \Exception
 	 */
 	public static function postCreateProject(Event $event): void
 	{
 		\Esites\ApiBundle\Scripts::postCreateProject($event);
+
+		$rootDir = $event->getComposer()->getConfig()->get('vendor-dir') . '/..';
+
+		self::copyEnvironmentVariables($rootDir);
+	}
+
+
+	/**
+	 * @param string $rootDir
+	 * @throws \Exception
+	 */
+	private static function copyEnvironmentVariables(string $rootDir): void
+	{
+		// Copy the .env.dist file to a .env file
+		copy($rootDir . '/.env.dist', $rootDir . '/.env');
+		copy($rootDir . '/.env.local.dist', $rootDir . '/.env.local');
+
+		$env = file_get_contents($rootDir . '/.env');
+		str_replace([
+			'{{APP_ENV}}',
+			'{{APP_SECRET}}',
+			'{{BASE_URL}}',
+		], [
+			'dev',
+			random_bytes(32),
+			'project.test'
+		], $env);
+
+		file_put_contents($rootDir . '/.env', $env);
 	}
 }
